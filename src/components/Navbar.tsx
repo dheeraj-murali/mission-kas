58073e24d309e746f73024617fd8be2e238f014b
Dheeraj Murali <410dheeraj@gmail.com>
2022-07-13 09:25:01 +0530

    feat: Updated responsiveness
    - removed unused images
    - updated navbar responsivness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,39 +12,40 @@ const navigation = [
 
 export const Navbar = () => {
   return (
-    <Popover>
-      <div className="bg-teal-900 relative pt-6 px-4 sm:px-6 lg:px-8">
-        <nav
-          className="relative flex items-center justify-between"
-          aria-label="Global"
-        >
-          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-            <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
-                <span className="sr-only">Workflow</span>
-                <Logo width={150} height={50} type="light" />
-              </a>
-              <div className="-mr-2 flex items-center md:hidden">
-                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
-                  <span className="sr-only">Open main menu</span>
-                  <MenuAlt1Icon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
+    <Popover
+      as="div"
+      className="bg-teal-900 md:shadow md:shadow-white relative pt-6 px-4 sm:px-6 lg:px-8"
+    >
+      <nav
+        className="relative flex items-center justify-between"
+        aria-label="Global"
+      >
+        <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <Logo width={180} height={50} type="light" />
+            </a>
+            <div className="-mr-2 flex items-center md:hidden">
+              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
+                <span className="sr-only">Open main menu</span>
+                <MenuAlt1Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
             </div>
           </div>
-          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-white font-medium hover:text-emerald-300 hover:border-b-2"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        </nav>
-      </div>
+        </div>
+        <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-white font-medium hover:text-emerald-300 hover:border-b-2"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </nav>
 
       <Transition
         as={Fragment}
@@ -62,7 +63,7 @@ export const Navbar = () => {
           <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
             <div className="px-5 pt-4 flex items-center justify-between">
               <div>
-                <Logo width={150} height={50} type="dark" />
+                <Logo width={180} height={50} type="dark" />
               </div>
               <div className="-mr-2">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
