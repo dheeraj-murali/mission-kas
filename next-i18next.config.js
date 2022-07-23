d439bb5be958b1a3b1f25129b6a7dcce52245c8c
Dheeraj Murali <410dheeraj@gmail.com>
2022-07-23 11:43:14 +0530

    feat: i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    i18n: {
+      defaultLocale: 'en_in',
+      locales: ['en_in', 'ml'],
+      localeDetection: false,
+    },
+  };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html className='scroll-smooth'>
+      <Html className="scroll-smooth">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -15,8 +14,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
